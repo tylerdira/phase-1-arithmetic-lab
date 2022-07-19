53b96e0dc628d195a3ce113837bb396f21fa2a26
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-07-19 02:53:26 -0700

    commit tree e42286e8e2dd898f10284ddabb90b36a2e339150

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 62
+let num2 = 1
+
+var multiply = num1 * num2
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 4
+let num4 = 1
+var mod = num3 / num4;
+
+var max = Math.max(1, 10, 20)
